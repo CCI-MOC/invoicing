8c683a5e637b1e0e77576fc2e0460016b7e14f69
Quan Pham <qmpham2019@gmail.com>
2025-06-03 11:35:27 -0400

    Added H100 GPU SUs to Lenovo invoice
    H100s have a Lenovo charge of 2.74. Slight refactoring
    done to allow different SU charges for each SU type

--- process_report/tests/unit/processors/test_lenovo_processor.py
+++ process_report/tests/unit/processors/test_lenovo_processor.py
@@ -8,14 +8,20 @@ class TestLenovoProcessor(TestCase):
     def test_process_lenovo(self):
         test_invoice = pandas.DataFrame(
             {
+                "SU Type": [
+                    "OpenShift GPUA100SXM4",
+                    "OpenStack GPUA100SXM4",
+                    "OpenStack GPUH100",
+                    "BM GPUH100",
+                    "CPU",
+                    "GPU",
+                ],
                 "SU Hours (GBhr or SUhr)": [1, 10, 100, 4, 432, 10],
             }
         )
         answer_invoice = test_invoice.copy()
-        answer_invoice["SU Charge"] = 1
-        answer_invoice["Charge"] = (
-            answer_invoice["SU Hours (GBhr or SUhr)"] * answer_invoice["SU Charge"]
-        )
+        answer_invoice["SU Charge"] = [1, 1, 2.74, 2.74, 0, 0]
+        answer_invoice["Charge"] = [1, 10, 274, 10.96, 0, 0]
 
         lenovo_proc = test_utils.new_lenovo_processor(data=test_invoice)
         lenovo_proc.process()
