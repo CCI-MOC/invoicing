3e416476e7f97f48146a34f3c2f630eba9fd6d18
marcoagonzales007 <marcoagonzales007@gmail.com>
2026-05-31 19:49:11 -0700

    Add unit tests for prepaycreditssnapshot

--- process_report/tests/unit/invoices/test_credits_snapshot.py
+++ process_report/tests/unit/invoices/test_credits_snapshot.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 import pandas
 
 from process_report.tests import util as test_utils
@@ -39,3 +39,43 @@ class TestCreditsSnapshot(TestCase):
         output_snapshot = new_prepayment_proc._get_prepay_credits_snapshot()
 
         assert answer_credits_snapshot.equals(output_snapshot)
+
+    def test_output_path(self):
+        inv = test_utils.new_prepay_credits_snapshot(invoice_month="2025-01")
+
+        assert inv.output_path == "NERC_Prepaid_Group-Credits-2025-01.csv"
+
+    def test_output_s3_key(self):
+        inv = test_utils.new_prepay_credits_snapshot(invoice_month="2025-01")
+        assert (
+            inv.output_s3_key
+            == "Invoices/2025-01/NERC_Prepaid_Group-Credits-2025-01.csv"
+        )
+
+    @mock.patch("process_report.util.get_iso8601_time")
+    def test_output_s3_archive_key(self, mock_time):
+        mock_time.return_value = "2025-01-01T00:00:00"
+        inv = test_utils.new_prepay_credits_snapshot(invoice_month="2025-01")
+        assert (
+            inv.output_s3_archive_key
+            == "Invoices/2025-01/Archive/NERC_Prepaid_Group-Credits-2025-01 2025-01-01T00:00:00.csv"
+        )
+
+    def test_prepare(self):
+        test_prepay_credits = pandas.DataFrame(
+            {"Month": ["2025-01"], "Group Name": ["G1"], "Credit": [100]}
+        )
+        test_prepay_contacts = pandas.DataFrame(
+            {
+                "Group Name": ["G1"],
+                "Group Contact Email": [""],
+                "MGHPCC Managed": ["Yes"],
+            }
+        )
+        inv = test_utils.new_prepay_credits_snapshot(
+            invoice_month="2025-01",
+            prepay_credits=test_prepay_credits,
+            prepay_contacts=test_prepay_contacts,
+        )
+        inv._prepare()
+        assert len(inv.export_data) == 1
